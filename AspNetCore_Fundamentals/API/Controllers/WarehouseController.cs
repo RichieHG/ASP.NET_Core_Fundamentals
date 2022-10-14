f5bb8ce46b7e1220a44c736bba6f972db53fb1ad
Ricardo Neftali Hernandez Gomez <neftali.hergom@gmail.com>
2022-10-13 20:05:53 -0500

    Feat: Intial Project

---
+++ AspNetCore_Fundamentals/API/Controllers/WarehouseController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    public class WarehouseController: Controller
+    {
+        public WarehouseController()
+        {
+        }
+
+        [HttpGet]
+        public IActionResult GetWarehouse()
+        {
+            return Ok("Hello World!");
+        }
+    }
+}
